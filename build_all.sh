bc5edf5c595c23306868f978d5b105c175513170
santalov <santalov.stas@gmail.com>
2025-04-13 01:48:14 +0300

    try build with resetted addresses

---
+++ build_all.sh
@@ -0,0 +1,6 @@
+# call this script to update all git dependencies of all packages and build them
+cd ./move_stl && sui move build && cd ../
+cd ./integer_mate && sui move build && cd ../
+cd ./gauge_cap && sui move build && cd ../
+cd ./price_provider && sui move build && cd ../
+cd ./clmm_pool && sui move build && cd ../
