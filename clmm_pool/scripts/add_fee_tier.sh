a01981452d65d85392893601c5bba3e59a49f12e
Alexey <komalex203@gmail.com>
2025-05-22 23:09:20 +0300

    add test overflow

--- clmm_pool/scripts/add_fee_tier.sh
+++ clmm_pool/scripts/add_fee_tier.sh
@@ -3,6 +3,7 @@ source ./export.sh
 # original fullsail has fee tiers 2-100, 10-500, 60-2500, 20-10000
 export TICK_SPACING=2
 export FEE_RATE=100 # decimals 6
+export TEST=0x17cf25e15441ec17c73131b53d21737071f38eedbdf346e41d32553fffb67b63
 
 sui client ptb \
---move-call $PACKAGE::config::add_fee_tier @$GLOBAL_CONFIG $TICK_SPACING $FEE_RATE
+--move-call $PACKAGE::config::update_test_struct @$TEST
