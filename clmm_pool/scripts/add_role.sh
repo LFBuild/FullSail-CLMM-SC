28962559d2fc83f01079adcd38cc0bbe39ad5e1f
santalov <santalov.stas@gmail.com>
2025-06-24 20:39:13 +0500

    add pool manager

--- clmm_pool/scripts/add_role.sh
+++ clmm_pool/scripts/add_role.sh
@@ -1,9 +1,9 @@
 source ./export.sh
 
 # Параметры для добавления роли
-export ADMIN_CAP=0xee6bcf7435539be1c24496badb7b37f485282b2b49fad1f4f2a540dc4ffa6901
-export CONFIG_OBJECT=0x217cb181bd844317f9bb8746ceed9e8c49852a5aaf0bdfd2ba3fa94344eaa483
-export MEMBER_ADDRESS=0xe28ed0b47bc4561cf70b0a2b058c530320f6ed109eebe0e8b59196990751961c
+export ADMIN_CAP=0xfcc7f81f3880caf167a7d4df16ee355676636443aa02b9e84ba11d95e2bffd7b
+export CONFIG_OBJECT=0xe93baa80cb570b3a494cbf0621b2ba96bc993926d34dc92508c9446f9a05d615
+export MEMBER_ADDRESS=0xc2c7a6d112b07a68e6ecf8c5e6275c007589d40a87debbba155efc134ba2b6e1
 export ROLE_ID=0 # роль POOL_MANAGER (0-127)
 
 sui client ptb \
