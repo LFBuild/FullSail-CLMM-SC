6300f71bd3a418211e1a5ac6bcdb76a5104755ad
Oleg Shatniuk <o.shatnuk@gmail.com>
2025-06-26 15:08:22 +0300

    add set display scripts

---
+++ clmm_pool/scripts/set_display_v2.sh
@@ -0,0 +1,12 @@
+source ./export.sh
+
+sui client ptb \
+--move-call \
+$PACKAGE::position::set_display_v2 \
+@$CONFIG_OBJECT \
+@$POSITION_PUBLISHER \
+"{description}" \
+"https://testnet.fullsail.finance/liquidity/{pool}/positions/{id}" \
+"https://app.fullsail.finance/static_files/fullsail_logo.png" \
+"https://fullsail.finance" \
+"FULLSAIL"
