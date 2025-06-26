5a006b48eef2793ccfbd94c6028fc29140ff565a
Oleg Shatniuk <o.shatnuk@gmail.com>
2025-06-26 15:46:19 +0300

    updated scripts

--- clmm_pool/scripts/set_display_v2_prod.sh
+++ clmm_pool/scripts/set_display_v2_prod.sh
@@ -1,12 +1,10 @@
 source ./export.sh
 
 sui client ptb \
---move-call \
-$PACKAGE::position::set_display_v2 \
-@$CONFIG_OBJECT \
+--move-call $PACKAGE::position::set_display_v2 @$GLOBAL_CONFIG \
 @$POSITION_PUBLISHER \
-"{description}" \
-"https://app.fullsail.finance/liquidity/{pool}/positions/{id}" \
-"https://app.fullsail.finance/static_files/fullsail_logo.png" \
-"https://fullsail.finance" \
-"FULLSAIL"
+'"{description}"' \
+'"https://app.fullsail.finance/liquidity/{pool}/positions/{id}"' \
+'"https://app.fullsail.finance/static_files/fullsail_logo.png"' \
+'"https://fullsail.finance"' \
+'"FULLSAIL"'
