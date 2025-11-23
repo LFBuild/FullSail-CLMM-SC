f8daa6b3d67497e97454b67b33614094a3f42610
santalov <santalov.stas@gmail.com>
2025-11-24 04:13:48 +0500

    update fee rate

--- clmm_pool/scripts/update_unstaked_fee_rate.sh
+++ clmm_pool/scripts/update_unstaked_fee_rate.sh
@@ -2,4 +2,4 @@ source ./export.sh
 source ./pools/pool_lofi_sui.sh
 
 sui client ptb \
---move-call $PACKAGE::pool::update_unstaked_liquidity_fee_rate "<$COIN_A,$COIN_B>" @$GLOBAL_CONFIG @$POOL 0
+--move-call $PACKAGE::pool::update_unstaked_liquidity_fee_rate "<$COIN_A,$COIN_B>" @$GLOBAL_CONFIG @$POOL 10000
