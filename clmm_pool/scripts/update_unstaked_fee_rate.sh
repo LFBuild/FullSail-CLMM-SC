4fa0db3a3b27fdc022bd2767445d6d1ee7453b92
santalov <santalov.stas@gmail.com>
2026-01-08 15:31:57 +0500

    more scripts

--- clmm_pool/scripts/update_unstaked_fee_rate.sh
+++ clmm_pool/scripts/update_unstaked_fee_rate.sh
@@ -1,5 +1,5 @@
 source ./export.sh
-source ./pools/pool_stsui_usdc.sh
+source ./pools/pool_stsui_wal.sh
 
 sui client ptb \
 --move-call $PACKAGE::pool::update_unstaked_liquidity_fee_rate "<$COIN_A,$COIN_B>" @$GLOBAL_CONFIG @$POOL 10000
