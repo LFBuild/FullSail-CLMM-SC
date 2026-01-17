61e9155067b8a888de889d6e5d3a098217a172dd
santalov <santalov.stas@gmail.com>
2026-01-17 15:57:02 +0500

    new pool

--- clmm_pool/scripts/add_role.sh
+++ clmm_pool/scripts/add_role.sh
@@ -1,8 +1,8 @@
 source ./export.sh
 
 # Параметры для добавления роли
-export MEMBER_ADDRESS=0xfed1c619fc8dd98367a0422ca9ef53c9825e2893d78dda822106d12687888fb3
-export ROLE_ID=2 # роль POOL_MANAGER (0-127)
+export MEMBER_ADDRESS=0x5629bc7f393bf242a5407c59f530a1150f6c863849a288bba801a88379fb966c
+export ROLE_ID=0 # роль POOL_MANAGER (0-127)
 
 sui client ptb \
 --sender @0xfed1c619fc8dd98367a0422ca9ef53c9825e2893d78dda822106d12687888fb3 --gas-coin @0xe6600100e23ccefd343837558242768b59dacca73b28132482e1b0ec6370c81b --gas-budget 100000000 --serialize-unsigned-transaction \
--- clmm_pool/scripts/update_fee_rate.sh
+++ clmm_pool/scripts/update_fee_rate.sh
@@ -1,7 +1,7 @@
 source ./export.sh
-source ./pools/pool_stsui_usdc.sh
+source ./pools/pool_deep_wal.sh
 
-export FEE_RATE=1750 # denom is 1000000
+export FEE_RATE=1000 # denom is 1000000
 
 sui client ptb \
 --move-call $PACKAGE::pool::update_fee_rate "<$COIN_A,$COIN_B>" @$GLOBAL_CONFIG @$POOL $FEE_RATE
