48ddee998cdf208d6c0f5f8c11f1a36e814bc396
santalov <santalov.stas@gmail.com>
2026-01-26 23:31:07 +0500

    new pools

--- clmm_pool/scripts/update_fee_rate.sh
+++ clmm_pool/scripts/update_fee_rate.sh
@@ -1,7 +1,7 @@
 source ./export.sh
-source ./pools/pool_usdz_usdc_2.sh
+source ./pools/pool_xaum_usdc.sh
 
-export FEE_RATE=500 # denom is 1000000
+export FEE_RATE=1000 # denom is 1000000
 
 sui client ptb \
 --move-call $PACKAGE::pool::update_fee_rate "<$COIN_A,$COIN_B>" @$GLOBAL_CONFIG @$POOL $FEE_RATE
