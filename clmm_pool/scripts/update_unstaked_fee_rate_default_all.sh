1122d0ba9dadb0e49e1bcce640343221029f696b
santalov <santalov.stas@gmail.com>
2026-03-12 21:28:13 +0500

    update unstaked fee rate

--- clmm_pool/scripts/update_unstaked_fee_rate_default_all.sh
+++ clmm_pool/scripts/update_unstaked_fee_rate_default_all.sh
@@ -1,37 +1,19 @@
 source ./export.sh
 
 # default_unstaked_fee_rate() = 72057594037927935 (sentinel value, falls through to global config rate)
-DEFAULT_RATE=72057594037927935
+RATE=2000
 
 update_pool() {
     source ./pools/pool_$1.sh
-    echo "--move-call $PACKAGE::pool::update_unstaked_liquidity_fee_rate <$COIN_A,$COIN_B> @$GLOBAL_CONFIG @$POOL $DEFAULT_RATE"
+    echo "--move-call $PACKAGE::pool::update_unstaked_liquidity_fee_rate <$COIN_A,$COIN_B> @$GLOBAL_CONFIG @$POOL $RATE"
 }
 
 sui client ptb \
 $(update_pool "alkimi_sui") \
 $(update_pool "axol_sui") \
-$(update_pool "deep_sui") \
-$(update_pool "deep_wal") \
-$(update_pool "eth_usdc") \
-$(update_pool "ika_sui") \
-$(update_pool "l0wbtc_usdc") \
-$(update_pool "lofi_sui") \
 $(update_pool "manifest_usdc") \
 $(update_pool "mmt_usdc") \
 $(update_pool "mystic_sui") \
-$(update_pool "sail_usdc") \
-$(update_pool "stsui_deep") \
-$(update_pool "stsui_usdc") \
-$(update_pool "stsui_wal") \
-$(update_pool "sui_usdc") \
 $(update_pool "tato_sui") \
 $(update_pool "up_sui") \
-$(update_pool "usdb_sui") \
-$(update_pool "usdb_usdc") \
-$(update_pool "usdt_usdc") \
-$(update_pool "usdz_usdc") \
-$(update_pool "wal_sui") \
-$(update_pool "wbtc_usdc") \
-$(update_pool "xaum_usdc") \
 $(update_pool "xbtc_l0wbtc")
--- clmm_pool/scripts/update_unstaked_fee_rate_global.sh
+++ clmm_pool/scripts/update_unstaked_fee_rate_global.sh
@@ -1,4 +1,4 @@
 source ./export.sh
 
 sui client ptb \
---move-call $PACKAGE::config::update_unstaked_liquidity_fee_rate @$GLOBAL_CONFIG 2000
+--move-call $PACKAGE::config::update_unstaked_liquidity_fee_rate @$GLOBAL_CONFIG 10000
