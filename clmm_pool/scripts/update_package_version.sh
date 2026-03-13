96880a6a5494c0e9c87f59f0e1b558d8dd3d34ce
santalov <santalov.stas@gmail.com>
2026-03-13 19:16:43 +0500

    update package v script

--- clmm_pool/scripts/update_package_version.sh
+++ clmm_pool/scripts/update_package_version.sh
@@ -1,5 +1,5 @@
 source ./export.sh
 
 sui client ptb \
---sender @0xfed1c619fc8dd98367a0422ca9ef53c9825e2893d78dda822106d12687888fb3 --gas-coin @0xe6600100e23ccefd343837558242768b59dacca73b28132482e1b0ec6370c81b --gas-budget 100000000 --serialize-unsigned-transaction \
---move-call $PACKAGE::config::update_package_version @$ADMIN_CAP @$GLOBAL_CONFIG 2
+--sender @0xfed1c619fc8dd98367a0422ca9ef53c9825e2893d78dda822106d12687888fb3 --gas-coin @0xcc7c6b958f68357cfb10ca6c74d0fcec14da7dfc9b68a191c5eccc3a08454c91 --gas-budget 100000000 --serialize-unsigned-transaction \
+--move-call $PACKAGE::config::update_package_version @$ADMIN_CAP @$GLOBAL_CONFIG 3
